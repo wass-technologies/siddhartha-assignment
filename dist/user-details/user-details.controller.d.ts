7c5ca4eaaa1ee5987598d5d51b156d03fea986ee
Siddhartha Majumder <siddhartha191122@gmail.com>
2025-03-21 18:57:00 +0530

    Staff Service

--- dist/user-details/user-details.controller.d.ts
+++ dist/user-details/user-details.controller.d.ts
@@ -1,12 +1,11 @@
 import { PaginationDto } from './dto/update-user-details';
-import { School } from './entities/user-detail.entity';
 import { SchoolService } from './user-details.service';
 import { Response } from 'express';
 import { Account } from 'src/account/entities/account.entity';
 export declare class SchoolController {
     private readonly schoolService;
     constructor(schoolService: SchoolService);
-    getSchoolDetails(user: Account): Promise<School>;
+    getSchoolDetails(user: Account): Promise<import("./entities/user-detail.entity").School>;
     getTotalClasses(paginationDto: PaginationDto, user: Account): Promise<{
         result: import("../class/entities/class.entity").ClassEntity[];
         total: number;
@@ -20,5 +19,5 @@ export declare class SchoolController {
     }>;
     getStudentById(studentId: string, user: Account): Promise<import("../student/entities/student.entity").Student>;
     generateSchoolListPdf(res: Response): Promise<void>;
-    assignSubAdmin(schoolId: string, subAdminId: string): Promise<School>;
+    assignSubAdmin(schoolId: string, subAdminId: string): Promise<import("./entities/user-detail.entity").School>;
 }
